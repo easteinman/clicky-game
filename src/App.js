0092280fbed98f079ce3811bd565c20a2ee11ee1
Eric <ericsteinman@me.com>
2018-04-20 14:03:30 -0500

    Went through and added some notes as I hadn't been doing that

--- src/App.js
+++ src/App.js
@@ -9,50 +9,56 @@ import Row from "./components/Row";
 import PeopleCard from "./components/PeopleCard";
 import Footer from "./components/Footer";
 
+// Create the variables that will be used in the app
 let topScore = 0;
 let Score = 0;
 let numberCorrect = 0;
 let Alert = "";
 
 class App extends Component {
-
+  // Setting this.state._____ to their variables. People being the people array in people.json
 	state = {
 		People,
     topScore,
 		numberCorrect,
     Alert
 	};
-
+  //setClicked from PeopleCard.js
+  // Setting the ID to the ID of the person clicked
   setClicked = id => {
     const People = this.state.People;
+    // Filter people array, set the ID of the cardClicked
     const cardClicked = People.filter(Person => Person.id === id);
-
+    //If a person already clicked is clicked again...
     if (cardClicked[0].clicked) {
-
+      // Reset number correct
       numberCorrect = 0;
+      // Set losing alert
       Alert = 'Boohoohoo. Try a malenky bit harder next time my droog.';
-
-
+      // Reset clicked boolean for the people
       for (let i = 0; i < People.length; i++) {
         People[i].clicked = false;
       }
-
+      // Set the above states
       this.setState({Alert});
       this.setState({numberCorrect});
       this.setState({People});
 
+      // If a person has not been clicked before, set clicked to true
     } else {
       cardClicked[0].clicked = true;
+      // Choosing to leave alert empty due to the Alert flashing
+      // Just want the losing alert to show and flash
       Alert = ""; 
-
+      // Increase numberCorrext by one
       numberCorrect = numberCorrect +1;
-
+      // If the number corrext exceeds the top score, set new top score
       if (numberCorrect > topScore) {
         topScore = numberCorrect;
         this.setState({Score});
         this.setState({topScore});
       }
-
+      // Randomly sort the people
       People.sort(() => {
         return 0.5 - Math.random();
       });
